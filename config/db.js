c51ed4fc8b6cad5623f45b08b608f8bac1f5046c
Rizvan337 <rizvanichu337@gmail.com>
2024-12-17 11:40:55 +0530

    connect mongodb to server for hosting

--- config/db.js
+++ config/db.js
@@ -3,8 +3,11 @@ const env = require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('DB Connected');
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+      console.log('DB Connected');
+
+    })
   } catch (error) {
     console.log('DB Connection error', error.message);
     process.exit(1);
